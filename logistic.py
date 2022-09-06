45951c3b29d0c175c0fdf52b52bbb19ec2305c1f
ASPP Student <student@localhost>
2022-09-06 12:03:09 +0200

    Implement iterate_f function

--- logistic.py
+++ logistic.py
@@ -1,2 +1,10 @@
 def logistic_map(x: float, r: float) -> float:
     return r*x*(1-x)
+
+def iterate_f(it: int, x, r) -> list[float]:
+    rv = []
+    for _ in range(it):
+        x = logistic_map(x, r)
+        rv.append(x)
+    return rv
+
